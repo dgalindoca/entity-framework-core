80f68dbc85fa41bcb12283e53ee0dac27f238f23
Daniel Felipe Galindo <DanielFelipe.Galindo@bofifederalbank.com>
2026-04-07 14:29:50 -0500

    Get all books

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -12,3 +12,15 @@ Console.WriteLine("Hello, World!");
 //        context.Database.Migrate();
 //    }
 //}
+
+GetAllBooks();
+
+void GetAllBooks()
+{
+    using var context = new ApplicationDbContext();
+    var books = context.Books.ToList();
+    foreach (var book in books)
+    {
+        Console.WriteLine($"{book.Title} - {book.ISBN}");
+    }
+}
