6d6a002a2c985e46a579c24b6e4b6802cc03c43c
dgalindoca <dgalindoca@unal.edu.co>
2026-05-16 18:02:22 -0500

    Setup EF Core in Web Project

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -27,11 +27,15 @@ namespace CodingWiki_DataAccess.Data
         public DbSet<Fluent_Publisher> Fluent_Publisher { get; set; }
         public DbSet<Fluent_BookAuthorMap> Fluent_BookAuthorMaps { get; set; }
 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=localhost;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;")
-                .LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name}, LogLevel.Information);
+            //options.UseSqlServer("Server=localhost;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;")
+            //    .LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name}, LogLevel.Information);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- CodingWiki_Web/Program.cs
+++ CodingWiki_Web/Program.cs
@@ -1,8 +1,12 @@
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'CodingWiki_WebContext' not found.")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
